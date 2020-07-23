8e754d71e19e176017cf6a1407c5e21041f9ca6c
Muhammed TOSUN <tosun.muhammed@outlook.com>
2020-07-23 12:41:08 +0300

    first commit

---
+++ src/main/java/org/learnspring/bootspring/BootspringApplication.java
@@ -0,0 +1,13 @@
+package org.learnspring.bootspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/learnspring/bootspring/BootspringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learnspring.bootspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
